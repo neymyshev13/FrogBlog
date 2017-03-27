55df1557cfafc303f7706c22869bfbb4c4279270
Artem Neymyshev <neymyshev15@gmail.com>
2017-03-27 13:23:39 +0300

    add text editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("turbolinks:load", function() {
+  tinymce.remove();
+  tinymce.init({ selector: 'textarea#post_body',
+  plugins: "codesample image media link code",
+  toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
+  });
+})
